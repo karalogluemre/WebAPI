1826f7cf68d8c9c82e1a5a6009026b8a09e060b1
Emre Karaloğlu <emrekaraloglu@hotmail.com>
2022-03-10 17:06:14 +0300

    UserDetail

--- Entities/Dtos/UserDtos/UserDetailDto.cs
+++ Entities/Dtos/UserDtos/UserDetailDto.cs
@@ -13,7 +13,6 @@ namespace Entities.Dtos.UserDtos
         public string UserName { get; set; }
         public string FirstName { get; set; }
         public string SurName { get; set; }
-        public string Password { get; set; }
         public string Gender { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string Email { get; set; }
