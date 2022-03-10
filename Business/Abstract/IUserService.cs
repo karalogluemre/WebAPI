08a1adcdef7de7cdd25d3314509b78b08a7b74b9
Emre Karaloğlu <emrekaraloglu@hotmail.com>
2022-03-10 17:05:49 +0300

    IUserService

---
+++ Business/Abstract/IUserService.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Dtos.UserDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<UserDetailDto>> GetListAsync();
+        Task<UserDto> GetByIdAsync(int id);
+        Task<UserDto> AddAsync(UserAddDto userAddDto);
+        Task<UserUpdateDto> UpdateAsync(UserUpdateDto userUpdateDto);
+        //Task<T> DeleteAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
